2d1bc3a31563b61fc44eecedb289a00a1968f7b0
youngha <pyh.uuhc@gmail.com>
2024-07-07 16:29:35 +0900

    update package

---
+++ error_wrapping/error_wrapping.go
@@ -0,0 +1,32 @@
+package errorwrapping
+
+import (
+	"errors"
+	"fmt"
+)
+
+// 정의된 기본 오류
+var ErrNotFound = errors.New("not found")
+
+// 오류를 래핑하는 함수
+func findItem(id int) error {
+	if id != 1 {
+		// ErrNotFound 오류를 래핑하여 반환
+		return fmt.Errorf("findItem: %w", ErrNotFound)
+	}
+	return nil
+}
+
+func main() {
+	err := findItem(2)
+	if err != nil {
+		// errors.Is를 사용하여 ErrNotFound 오류인지 확인
+		if errors.Is(err, ErrNotFound) {
+			fmt.Println("Item not found")
+		} else {
+			fmt.Println("An unexpected error occurred:", err)
+		}
+	} else {
+		fmt.Println("Item found")
+	}
+}
